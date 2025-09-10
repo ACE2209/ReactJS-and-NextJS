1ee358919021ed1c8e71c6bcd8c193d592a35b93
Truan dep troai <trantnce180829@fpt.edu.vn>
2025-09-11 06:51:20 +0700

    update second

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 // import Output from "./bai/1/bai1";
-import Output from "./bai/2/bai2";
+// import Output from "./bai/2/bai2";
+// import Output from "./bai/3/bai3";
+import Output from "./page/OutputComponent";
 
 function App() {
   return (
---
+++ src/bai/3/bai3.jsx
@@ -0,0 +1,42 @@
+// tối ưu hóa code của bài 2 bằng cách sử dụng file.js
+import { myData } from "../../../data.js";
+import PropTypes from "prop-types";
+
+function MainContent({image, title, desc}) {
+  return (
+    // sử dụng destructuring để giảm thiểu gọi props của bài 2
+    <li>
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <p>{desc}</p>
+    </li>
+  );
+}
+
+MainContent.propTypes = {
+  image: PropTypes.string.isRequired, // kiểu chuỗi . bắt buộc phải có
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string,
+};
+
+function Output() {
+  console.log(myData);
+
+  return (
+    <>
+      <main>
+        <section id="core-concepts">
+          <h2>Khái niệm chính trong React</h2>
+          <ul>
+            {/* sử dụng toán tử spread operator để "trải" (spread) các thuộc tính của một object  */}
+            <MainContent {...myData[0]} />
+            <MainContent {...myData[1]} />
+            <MainContent {...myData[2]} />
+            <MainContent {...myData[3]} />
+          </ul>
+        </section>
+      </main>
+    </>
+  );
+}
+export default Output;
