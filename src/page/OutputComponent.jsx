24fbd0b5257149b9ad1ed3e9c3403918ace8f7e6
Truan dep troai <trantnce180829@fpt.edu.vn>
2025-09-11 08:30:42 +0700

    update second

--- src/page/OutputComponent.jsx
+++ src/page/OutputComponent.jsx
@@ -2,6 +2,8 @@ import MainContent from "../components/MainContent.jsx";
 import Header from "../components/Header/Header.jsx";
 import {myData} from "../../data.js";
 
+import TabButton from "../components/TabButton.jsx";
+
 function Output() {
   return (
     <>
@@ -15,6 +17,20 @@ function Output() {
             <MainContent {...myData[2]} />
             <MainContent {...myData[3]} />
           </ul>
+        </section>
+
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            {/* Dùng props.children để hiển thị nội dung mà bạn đặt giữ cặp thẻ TabButton khi sử dụng component này */}
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+            
+            {/* <TabButton>Button1</TabButton> */}
+            {/* <TabButton batky="Components"></TabButton> */}
+          </menu>
         </section>
       </main>
     </>
